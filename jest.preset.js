bdba32ba42e6accc916aa393eaff9006e1249159
alwick <awick@reinventauctions.com>
2025-12-16 17:55:43 -0600

    Show code coverage on pull request.

--- jest.preset.js
+++ jest.preset.js
@@ -12,6 +12,6 @@ module.exports = {
     ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageReporters: ['html', 'text'],
+  coverageReporters: ['html', 'text', 'json', 'json-summary'],
 };
 
